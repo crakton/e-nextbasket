dadf5426b3269fd624a2c24c7ebba9dfc6143f4c
crakton <redemptionjonathan1@gmail.com>
2024-01-21 23:32:34 +0100

    #Update: Assets and Tw utils

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["*", "https://dummyjson.com", "https://cdn.dummyjson.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.dummyjson.com",
+        pathname: "*/**",
+      },
+    ],
   },
 };
 
